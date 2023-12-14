d183a79a4dabc375dd6e323574fe21c9d9410b57
sneha1995-tech <153805798+sneha1995-tech@users.noreply.github.com>
2023-12-14 13:21:58 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
  current_time = datetime.datetime.now()
- print ("timestamp:" , + current_time)
+ print ("timestamp:" + current_time)
 
  IP = socket.gethostbyname(hostname)
- print("ip:" , + IP)
+ print("ip:" + IP)
